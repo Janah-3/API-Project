70c380c8f6fa9d749e633d053d701f7010adf4fa
jana <jana.ayoub.004@gmail.com>
2025-07-05 00:37:18 +0300

    Generic repository interface and class

--- api/Program.cs
+++ api/Program.cs
@@ -19,6 +19,8 @@ namespace api
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
